271b36821c4265854210dd2ae82e25e19399e936
Ahsan Habib <ahredoan@gmail.com>
2020-01-07 18:08:43 +0600

    update payment option

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Item, OrderItem, Order, BillingAddress
+from .models import Item, OrderItem, Order, BillingAddress, Payment
 
 # Register your models here.
 
@@ -7,3 +7,4 @@ admin.site.register(Item)
 admin.site.register(OrderItem)
 admin.site.register(Order)
 admin.site.register(BillingAddress)
+admin.site.register(Payment)
